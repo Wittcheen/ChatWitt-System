6558b85be66f8e5de19ab9282a54d7e6ba32b616
Wittchen <wittchenchris@gmail.com>
2024-09-13 11:24:26 +0200

    Implemented news command and changed some messages

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -12,7 +12,7 @@ SAY_MODAL = interactions.Modal(
     title = "Send a message as the bot",
     *[interactions.InputText(
         custom_id = "say_message", required = True, min_length = 1, max_length = 3500,
-        style = interactions.TextStyles.PARAGRAPH, label = "Write the message the bot should send"
+        style = interactions.TextStyles.PARAGRAPH, label = "Message:"
     )]
 )
 
@@ -23,11 +23,11 @@ class Miscellaneous(interactions.Extension):
     #region - PURGE COMMAND
     @interactions.slash_command(name = "purge", description = "deletes the amount of messages",
         default_member_permissions = interactions.Permissions.MANAGE_MESSAGES, scopes = [id_map["server_id"]])
-    @interactions.slash_option(name = "amount", description = "write the amount to delete", opt_type = interactions.OptionType.INTEGER)
+    @interactions.slash_option(name = "amount", description = "specify the amount to delete", opt_type = interactions.OptionType.INTEGER)
     async def clear(self, ctx: interactions.SlashContext, amount: int = 1):
         await ctx.channel.purge(deletion_limit = amount)
-        message = await ctx.send(content = f"Deleted {amount} message(s) from this channel!", ephemeral = True)
-        await ctx.delete(message.id)
+        msg = await ctx.send(content = f"Deleted {amount} message(s) from this channel!", ephemeral = True)
+        await ctx.delete(msg.id)
     #endregion
 
     #region - SAY COMMAND
@@ -38,8 +38,8 @@ class Miscellaneous(interactions.Extension):
     @interactions.modal_callback("say_form")
     async def say_modal_response(self, ctx: interactions.ModalContext, say_message: str):
         await ctx.channel.send(content = say_message)
-        message = await ctx.send(content = "Completed.", ephemeral = True)
-        await ctx.delete(message.id)
+        msg = await ctx.send(content = "_The message has been sent._", ephemeral = True)
+        await ctx.delete(msg.id)
     #endregion
 
 def setup(client):
