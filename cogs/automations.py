c6a15c355d65eb06c451a39e8da35b76ef0f28f5
Wittchen <wittchenchris@gmail.com>
2024-10-03 14:30:51 +0200

    Made a listener for when members accepts the rules

---
+++ cogs/automations.py
@@ -0,0 +1,19 @@
+import interactions
+
+import yaml
+with open("./yaml/id_map.yaml") as config_file:
+    id_map = yaml.safe_load(config_file)
+
+class Automations(interactions.Extension):
+    def __init__(self, client):
+        self.client: interactions.Client = client
+
+    #region - MEMBER UPDATE LISTENER
+    @interactions.listen()
+    async def on_member_update(self, member_update: interactions.events.MemberUpdate):
+        if member_update.before.pending and not member_update.after.pending:
+            await member_update.after.add_role(id_map["member_role"])
+    #endregion
+
+def setup(client):
+    Automations(client)
